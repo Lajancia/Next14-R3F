51712e40284df542a67e6e0caf012f89a3b6ea6b
Soomin <soomin.hwang@illuminarean.com>
2024-11-25 16:29:22 +0900

    refactor : 컨텐츠 css 스타일링

--- panda.config.mjs
+++ panda.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from "@pandacss/dev";
+import { defineConfig } from '@pandacss/dev'
 
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
 
   // Where to look for your css declarations
-  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+  include: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
 
   // Files to exclude
   exclude: [],
@@ -14,7 +14,8 @@ export default defineConfig({
   theme: {
     extend: {},
   },
+  jsxFramework: 'react',
 
   // The output directory for your css system
-  outdir: "styled-system",
-});
+  outdir: 'styled-system',
+})
--- src/parts/keyboard/Info.jsx
+++ src/parts/keyboard/Info.jsx
@@ -1,15 +1,44 @@
 import { css } from 'styled-system/css'
+import Image from 'next/image'
 
 const Info = () => {
   return (
     <div className={StyledInfoWrapper}>
-      <div>Modern Art</div>
-      <h1>MACRO KEYBOARD</h1>
-      <p>2024-10-19</p>
-      <article>
+      <div className={StyledCategory}>Modern Art</div>
+      <h1 className={StyledTitle}>MACRO KEYBOARD</h1>
+      <div className={StyledCircle}>
+        <button className={circleButton({ color: '#fff' })} />
+        <button className={circleButton({ color: '#000' })} />
+        <button className={circleButton({ color: '#F99417' })} />
+      </div>
+      <p className={StyledDate}>2024-10-19</p>
+      <article className={StyledText}>
         Bla nskdanlkwdnwa asdanwdalsndalwd . asdnjawd askdjawjdad asdna asljdnadw asdaiawoif asdaowijaoijd
         asidjaowdjowijd aosidoawjdo oasdiajwdiaojd aojdoawdiwo aosdlanosd. aosidjaw. aosdaiowdi. aosjdaowdjidjaoasd.
       </article>
+      <div className={StyledImages}>
+        <Image
+          src='/img/keyboard/keyboard-1.png'
+          alt='Example Image'
+          width={1000}
+          height={1000}
+          className={StyledImage}
+        />
+        <Image
+          src='/img/keyboard/keyboard-2.png'
+          alt='Example Image'
+          width={1000}
+          height={1000}
+          className={StyledImage}
+        />
+        <Image
+          src='/img/keyboard/keyboard-3.png'
+          alt='Example Image'
+          width={1000}
+          height={1000}
+          className={StyledImage}
+        />
+      </div>
     </div>
   )
 }
@@ -17,9 +46,64 @@ export default Info
 
 const StyledInfoWrapper = css({
   display: 'flex',
+  width: '90%',
   flexDirection: 'column',
   justifyContent: 'center',
   padding: '5rem',
-  height: '100%',
+  marginTop: '15%',
+  height: '75%',
   color: 'white',
+  gap: '1rem',
+})
+
+const StyledCategory = css({
+  fontSize: '2rem',
+  lineHeight: '0.1rem',
+  color: 'white',
+})
+
+const StyledTitle = css({
+  fontSize: '5rem',
+  lineHeight: '100%',
+  color: 'white',
+})
+
+const StyledCircle = css({
+  display: 'flex',
+  gap: '1rem',
+})
+
+const StyledDate = css({
+  fontSize: '2.5rem',
+  color: 'white',
+})
+
+const StyledText = css({
+  fontSize: '1.5rem',
+  color: 'white',
+})
+
+const circleButton = (props) =>
+  css({
+    width: '3rem',
+    height: '3rem',
+    backgroundColor: props.color,
+    border: 'none',
+    borderRadius: '50%',
+    textAlign: 'center',
+    lineHeight: '4.4rem',
+    fontSize: '1.5rem',
+    cursor: 'pointer',
+  })
+
+const StyledImages = css({
+  display: 'flex',
+  justifyContent: 'space-between',
+  gap: '1rem',
+})
+
+const StyledImage = css({
+  width: '25%',
+  height: '16rem',
+  objectFit: 'cover',
 })
