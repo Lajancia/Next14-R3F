d2491161fd3cb3dedb4f82724719345283fcecc3
Soomin <soomin.hwang@illuminarean.com>
2025-01-30 19:59:45 +0900

    fix : theme color 수정

--- src/components/canvas/Boxes.tsx
+++ src/components/canvas/Boxes.tsx
@@ -10,7 +10,7 @@ function Box({ text, ...props }: any) {
   const ref = useRef()
   // const black = useMemo(() => new Color('transparent'), [])
   const orange = useMemo(() => new Color('orange'), [])
-  const [theme, setTheme] = useState(Cookies.get('theme') === 'dark' ? 'white' : 'black')
+  const [theme, setTheme] = useState(Cookies.get('theme') === 'light' ? 'black' : 'white')
   const [hovered, setHovered] = useState(false)
 
   useEffect(() => {
