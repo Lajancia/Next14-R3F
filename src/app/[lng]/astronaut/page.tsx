5450dd2b1f8e286880af36edecd47ccb49f20fc1
lajancia <kie6974@gmail.com>
2025-02-17 14:38:03 +0900

    fix : animation control 이슈 해결

--- src/app/[lng]/astronaut/page.tsx
+++ src/app/[lng]/astronaut/page.tsx
@@ -3,7 +3,7 @@ import { Suspense, useEffect, useState, useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { useGLTF, useTexture, useAnimations } from '@react-three/drei'
 import { easing } from 'maath'
-
+import { css } from '../../../../styled-system/css'
 function Rig() {
   return useFrame((state, delta) => {
     easing.damp3(state.camera.position, [1 + state.mouse.x / 4, 1.5 + state.mouse.y / 4, 2.5], 0.2, delta)
@@ -11,13 +11,64 @@ function Rig() {
 }
 
 export default function App() {
+  const [animationSetting, setAnimationSetting] = useState('standSit')
+  return (
+    <>
+      <div className={StyledButtonWrapper}>
+        <button className={StyledButton} onClick={() => setAnimationSetting('standSit')}>
+          stand and sit
+        </button>
+        <button className={StyledButton} onClick={() => setAnimationSetting('walking')}>
+          walk
+        </button>
+        <button className={StyledButton} onClick={() => setAnimationSetting('sitting')}>
+          sit
+        </button>
+      </div>
+      <div className={StyledModelContainer}>
+        <ControlModel animationSet={animationSetting} />
+      </div>
+    </>
+  )
+}
+
+const StyledModelContainer = css({
+  width: '100vw',
+  height: '100dvh',
+  zIndex: 0,
+})
+const StyledButtonWrapper = css({
+  position: 'absolute',
+  width: '100vw',
+  height: '100dvh',
+  zIndex: 1,
+
+  lg: { width: '100vw' },
+  xl: { width: '50vw' },
+})
+
+const StyledButton = css({
+  width: '10rem',
+  padding: '1rem',
+  marginLeft: '0.5rem',
+  border: '1px solid',
+  borderColor: 'MainText',
+  color: 'MainText',
+  borderRadius: '10px',
+  zIndex: 10,
+  '& hover': {
+    backgroundColor: 'white',
+  },
+})
+
+const ControlModel = ({ animationSet }: { animationSet: string }) => {
   return (
     <Canvas shadows camera={{ position: [1, 1.5, 2.5], fov: 50 }}>
       <ambientLight />
       <directionalLight position={[-5, 5, 5]} castShadow shadow-mapSize={1024} />
       <group position={[0, -1, 0]}>
         <Suspense fallback={null}>
-          <Model />
+          <Model animationSet={animationSet} />
         </Suspense>
       </group>
       <mesh rotation={[-0.5 * Math.PI, 0, 0]} position={[0, -1, 0]} receiveShadow>
@@ -35,6 +86,7 @@ function Model(props) {
   const { scene, nodes, animations, materials } = useGLTF('/astronaut_animation.glb')
   scene.castShadow = true
   scene.receiveShadow = true
+
   const { ref, actions, names } = useAnimations(animations)
 
   const [index, setIndex] = useState(0)
@@ -42,21 +94,14 @@ function Model(props) {
   // Change animation when the index changes
   // @ts-expect-error type ignore
   useEffect(() => {
-    console.log('name', names, index, names[index])
+    console.log('name', names, index, props.animationSet)
     console.log(materials, materials.CLAY)
 
     // @ts-expect-error type ignore
-    actions[names[index]].reset().fadeIn(0.5).play()
+    actions[props.animationSet].reset().fadeIn(0.5).play()
     // @ts-expect-error type ignore
-    return () => actions[names[index]].fadeOut(0.5)
-  }, [index, actions, names])
-
-  useFrame((state, delta) => {
-    // @ts-expect-error type ignore
-    easing.damp3(halo.current.scale, hovered ? 1.15 : 1, 0.2, delta)
-    // @ts-expect-error type ignore
-    easing.dampC(halo.current.material.color, hovered ? 'hotpink' : 'aquamarine', 0.2, delta)
-  })
+    return () => actions[props.animationSet].fadeOut(0.5)
+  }, [index, actions, names, props.animationSet])
 
   return (
     <group ref={ref} {...props} dispose={null}>
@@ -90,9 +135,6 @@ function Model(props) {
         ref={halo}
         receiveShadow
         position={[0, 1, -1]}
-        onClick={(e: any) => {
-          setIndex(Math.trunc((index + 1) % names.length))
-        }}
       >
         <circleGeometry args={[1, 64]} />
         <meshStandardMaterial />
