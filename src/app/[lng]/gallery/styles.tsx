2683efeac2e5f456f395300aa35ba454d82251a5
lajancia <kie6974@gmail.com>
2025-04-20 18:04:22 +0900

    fix height fix

--- src/app/[lng]/gallery/styles.tsx
+++ src/app/[lng]/gallery/styles.tsx
@@ -1,7 +1,7 @@
 import { css } from '../../../../styled-system/css'
 
 export const StyledHeaderContainer = css({ width: '100%', height: '20vh' })
-export const StyledFlexContainer = css({ width: '100vw', height: '80vh' })
+export const StyledFlexContainer = css({ width: '100vw', height: '80dvh' })
 export const StyledFooter = css({
   overflow: 'hidden',
   zIndex: 1,
